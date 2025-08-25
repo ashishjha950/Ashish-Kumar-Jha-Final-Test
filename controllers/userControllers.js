e7a25ba7f4012f80089a6e8c4aaec38de4030d69
ashishjha950 <ashishjha97099@gmail.com>
2025-08-25 15:02:14 +0530

    user route setup

---
+++ controllers/userControllers.js
@@ -0,0 +1,17 @@
+import userModel from "../models/userModel.js";
+
+export const registerUser = async (req,res) => {
+ try{
+    
+ }catch(err){
+    return res.status({message:"Server Error"})
+ } 
+}
+
+export const loginUser = async (req,res) => {
+ try{
+    
+ }catch(err){
+    return res.status({message:"Server Error"})
+ } 
+}
